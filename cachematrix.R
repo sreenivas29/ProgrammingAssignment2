840c0211ee5eb41ab6110647e60046e2ce6e9dd6
sreenivas29 <51162520+sreenivas29@users.noreply.github.com>
2021-07-07 10:51:40 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,11 @@
-## Put comments here that give an overall description of what your
-## functions do
+
 
 ## Write a short comment describing this function
-
+##cacheSolve is a function which computes the inverse of the special "matrix"returned by makeCacheMatrix above.
+#If the inverse has already been calculated(and the matrix has not changed), then the cachesolve should retrieve theinverse from the cache
+## I simply set the input x as a matrix
+## and then set the solved value "s" as a null
+## then I changed every reference to "mean" to "solve"
 makeCacheMatrix <- function(x = matrix()) {
         s <- NULL
   set <- function(y) {
@@ -18,6 +21,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 }
+## Same here, changed "mean" to "solve" and "m" to "s"
+##cacheSolve is a function which computes the inverse of the special "matrix"
+##returned by makeCacheMatrix above. If the inverse has already been calculated
+##(and the matrix has not changed), then the cachesolve should retrieve the inverse from cache
 cacheSolve <- function(x, ...) {
   s <- x$getsolve()
   if(!is.null(s)) {
